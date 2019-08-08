1fd404d63b4ee9ec4cf812d7bbc0f8ab33792ce2
jess <jlfinlayson4@gmail.com>
2019-08-08 14:19:48 -0400

    created routes in burgers_controller.js

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,53 @@
+// Import Express
+var express = require("express");
+
+// Import the model (burger.js)
+var burger = require("../models/burger.js");
+
+// Require router
+var router = express.Router();
+
+// Create routes
+
+// Get all burgers
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+// Insert new burger
+router.post("/api/burgers", function(req, res) {
+    burger.create([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function(result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+// Update burger
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+// Export routes
+module.exports = router;
+
