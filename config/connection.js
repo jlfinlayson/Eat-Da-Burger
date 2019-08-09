5c1448c9b73d513e224e9b360b14fcdc44e80a56
jess <jlfinlayson4@gmail.com>
2019-08-08 20:56:14 -0400

    updated spelling error

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,10 @@ var mysql = require("mysql");
 
 var connection;
 
-if(process.env.JAWSDB_URL) {
+if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else{ mysql.createConnection({
+} else{ 
+    connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
